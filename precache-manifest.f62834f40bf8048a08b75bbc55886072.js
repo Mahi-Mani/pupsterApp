5357e8eca5b5f9269e65e70229aac4c2e1c1e1f1
Mahi Mani <m.mahishamani@gmail.com>
2020-02-06 11:40:54 -0800

    Updates

---
+++ precache-manifest.f62834f40bf8048a08b75bbc55886072.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "122268efc9e324bf6a1ec76e56b2c509",
+    "url": "/pupsterApp/index.html"
+  },
+  {
+    "revision": "a048df4550a5a2bb9946",
+    "url": "/pupsterApp/static/css/main.d9a7a9b8.chunk.css"
+  },
+  {
+    "revision": "8c758e9f8ce559a081d2",
+    "url": "/pupsterApp/static/js/2.8072f32e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/pupsterApp/static/js/2.8072f32e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a048df4550a5a2bb9946",
+    "url": "/pupsterApp/static/js/main.6c460ceb.chunk.js"
+  },
+  {
+    "revision": "570bbf37df7069992fb4",
+    "url": "/pupsterApp/static/js/runtime-main.167c2c41.js"
+  }
+]);
